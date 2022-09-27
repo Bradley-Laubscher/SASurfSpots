91e3f23759ae23b0902b124a812409310e83fa76
Bradley-Laubscher <bradleylaubscher@gmail.com>
2022-09-27 17:27:21 +0200

    markers styling

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -10,79 +10,79 @@ app.controller('MainController', ['$scope', function($scope) {
         {
             lat: -34.030868,
             lng: 24.932402,
-            name: "Jeffreys Bay",
+            message: "Jeffreys Bay",
             img: "../../images/Jeffreys-Bay.webp"
         },
         {
             lat: -34.136998,
             lng: 18.327325,
-            name: "Kommetjie",
+            message: "Kommetjie",
             img: "../../images/Kommetjie.jpg"
         },
         {
             lat: -34.047104,  
             lng: 18.354142,
-            name: "Dungeons",
+            message: "Dungeons",
             img: "../../images/Dungeons.jpg"
         },
         {
             lat: -32.312679, 
             lng: 18.330055,
-            name: "Elands Bay",
+            message: "Elands Bay",
             img: "../../images/Elands-Bay.jpg"
         },
         {
             lat: -34.376042,  
             lng: 21.429591,
-            name: "Stillbaai",
+            message: "Stillbaai",
             img: "../../images/Stillbaai.jpg"
         },
         {
             lat: -34.004923,  
             lng: 22.549891,
-            name: "Victoria Bay",
+            message: "Victoria Bay",
             img: "../../images/Victoria-Bay.jpg"
         },
         {
             lat: -34.205817,   
             lng: 24.834960,
-            name: "Cape St Francis",
+            message: "Cape St Francis",
             img: "../../images/Cape-St-Francis.jpg"
         },
         {
             lat: -29.839000,    
             lng: 31.037936,
-            name: "Battery Beach",
+            message: "Battery Beach",
             img: "../../images/Battery-Beach.jpg"
         },
         {
             lat: -34.107893,  
             lng:  18.816643,
-            name: "Strand",
+            message: "Strand",
             img: "../../images/Strand.jpg"
         },
         {
             lat: -33.793685,  
             lng:  18.456330,
-            name: "Big Bay",
+            message: "Big Bay",
             img: "../../images/Big-Bay.jpg"
         },
         {
             lat: -32.995888,   
             lng:  27.951894,
-            name: "Nahoon Reef",
+            message: "Nahoon Reef",
             img: "../../images/Nahoon-Reef.jpg"
         },
         {
             lat: -33.599121,   
             lng:  26.906256,
-            name: "Port Alfred",
+            message: "Port Alfred",
             img: "../../images/Port-Alfred.jpg"
         },
         {
             lat: -31.984297,  
             lng:  29.151997,
-            name: "Coffee Bay",
+            message: "Coffee Bay",
             img: "../../images/Coffee-Bay.jpg"
         },
 
--- js/directives/spotInfo.js
+++ js/directives/spotInfo.js
@@ -5,20 +5,40 @@ app.directive('spotInfo', function() {
             info: '='
         },
         templateUrl: 'js/directives/spotInfo.html',
-        // link: function(scope, element, attrs) {
-        //     scope.buttonText = "View Spot",
-        //     scope.viewed = false,
+        link: function(scope, element, attrs) {
+            let toggle = false;
+            let markerIcon = document.querySelector(".leaflet-marker-icon");
+            
+            
+            scope.view = function() {
+                element.toggleClass('selected');
+                
+                if (!toggle) {
+                    markerIcon.style.opacity = '1';
+                    markerIcon.style.filter = "grayscale(0)";
+                    toggle = true;
+                } else {
+                    markerIcon.style.opacity = '0.4';
+                    markerIcon.style.filter = "grayscale(0.8)";
+                    toggle = false;
+                };
+            };
 
-        //     scope.view = function() {
-        //         element.toggleClass('btn-active');
-        //         if (scope.viewed) {
-        //             scope.buttonText = "view Spot";
-        //             scope.viewed = false;
-        //         } else {
-        //             scope.buttonText = "Back";
-        //             scope.viewed = true;
-        //         }
-        //     }
-        // }
-    };
+            scope.scroll = function(event) {
+                if (!event.deltaY) {
+                    return;
+                };
+                
+                event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+                event.preventDefault();
+            }
+        }
+    }
 });
+
+
+
+
+
+// dont know if I can change color - markers are imported icons - wthout css properties
+// iconSize is a property used to scale the size of the icons.
