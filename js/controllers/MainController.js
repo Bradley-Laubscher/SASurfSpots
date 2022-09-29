d333d161a139f4a4facb3cf02fb8627ae7575d36
Bradley-Laubscher <bradleylaubscher@gmail.com>
2022-09-29 12:07:00 +0200

    added marker highlighing when clicking on image

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,79 +11,92 @@ app.controller('MainController', ['$scope', function($scope) {
             lat: -34.030868,
             lng: 24.932402,
             message: "Jeffreys Bay",
-            img: "../../images/Jeffreys-Bay.webp"
+            img: "../../images/Jeffreys-Bay.webp",
+            isActive: false,
         },
         {
             lat: -34.136998,
             lng: 18.327325,
             message: "Kommetjie",
-            img: "../../images/Kommetjie.jpg"
+            img: "../../images/Kommetjie.jpg",
+            isActive: false,
         },
         {
             lat: -34.047104,  
             lng: 18.354142,
             message: "Dungeons",
-            img: "../../images/Dungeons.jpg"
+            img: "../../images/Dungeons.jpg",
+            isActive: false,
         },
         {
             lat: -32.312679, 
             lng: 18.330055,
             message: "Elands Bay",
-            img: "../../images/Elands-Bay.jpg"
+            img: "../../images/Elands-Bay.jpg",
+            isActive: false,
         },
         {
             lat: -34.376042,  
             lng: 21.429591,
             message: "Stillbaai",
-            img: "../../images/Stillbaai.jpg"
+            img: "../../images/Stillbaai.jpg",
+            isActive: false,
         },
         {
             lat: -34.004923,  
             lng: 22.549891,
             message: "Victoria Bay",
-            img: "../../images/Victoria-Bay.jpg"
+            img: "../../images/Victoria-Bay.jpg",
+            isActive: false,
         },
         {
             lat: -34.205817,   
             lng: 24.834960,
             message: "Cape St Francis",
-            img: "../../images/Cape-St-Francis.jpg"
+            img: "../../images/Cape-St-Francis.jpg",
+            isActive: false,
         },
         {
             lat: -29.839000,    
             lng: 31.037936,
             message: "Battery Beach",
-            img: "../../images/Battery-Beach.jpg"
+            img: "../../images/Battery-Beach.jpg",
+            isActive: false,
         },
         {
             lat: -34.107893,  
             lng:  18.816643,
             message: "Strand",
-            img: "../../images/Strand.jpg"
+            img: "../../images/Strand.jpg",
+            isActive: false,
         },
         {
             lat: -33.793685,  
             lng:  18.456330,
             message: "Big Bay",
-            img: "../../images/Big-Bay.jpg"
+            img: "../../images/Big-Bay.jpg",
+            isActive: false,
         },
         {
             lat: -32.995888,   
             lng:  27.951894,
             message: "Nahoon Reef",
-            img: "../../images/Nahoon-Reef.jpg"
+            img: "../../images/Nahoon-Reef.jpg",
+            isActive: false,
         },
         {
             lat: -33.599121,   
             lng:  26.906256,
             message: "Port Alfred",
-            img: "../../images/Port-Alfred.jpg"
+            img: "../../images/Port-Alfred.jpg",
+            isActive: false,
         },
         {
             lat: -31.984297,  
             lng:  29.151997,
             message: "Coffee Bay",
-            img: "../../images/Coffee-Bay.jpg"
+            img: "../../images/Coffee-Bay.jpg",
+            isActive: false,
         },
     ];
 }]);
--- js/directives/spotInfo.js
+++ js/directives/spotInfo.js
@@ -6,23 +6,174 @@ app.directive('spotInfo', function() {
         },
         templateUrl: 'js/directives/spotInfo.html',
         link: function(scope, element, attrs) {
-            let toggle = false;
-            let markerIcon = document.querySelector(".leaflet-marker-icon");
-            
-            
-            scope.view = function() {
+            const markerIcons = document.querySelectorAll(".leaflet-marker-icon");
+            let allMarkers = [];
+            allMarkers.push(...markerIcons);
+
+            scope.select = function() {
                 element.toggleClass('selected');
+                const message = scope.info.message;
+
+                // const markerIndex = allMarkers.indexOf(message);
+
+                // const markerStyle = function() {
+                //     if (!scope.info.isActive) {
+                //         allMarkers[markerIndex].style.opacity = "1";
+                //         allMarkers[markerIndex].style.filter = "grayscale(0)";
+                //         scope.info.isActive = true;
+                //     } else {
+                //         allMarkers[markerIndex].style.opacity = "0.2";
+                //         allMarkers[markerIndex].style.filter = "grayscale(0.8)";
+                //         scope.info.isActive = false;
+                //     }
+                // };          
                 
-                if (!toggle) {
-                    markerIcon.style.opacity = '1';
-                    markerIcon.style.filter = "grayscale(0)";
-                    toggle = true;
-                } else {
-                    markerIcon.style.opacity = '0.4';
-                    markerIcon.style.filter = "grayscale(0.8)";
-                    toggle = false;
-                };
+                switch (message) {
+                    case "Jeffreys Bay": 
+                        if (!scope.info.isActive) {
+                            allMarkers[0].style.opacity = "1";
+                            allMarkers[0].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[0].style.opacity = "0.2";
+                            allMarkers[0].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Kommetjie": 
+                        if (!scope.info.isActive) {
+                            allMarkers[1].style.opacity = "1";
+                            allMarkers[1].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[1].style.opacity = "0.2";
+                            allMarkers[1].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Dungeons": 
+                        if (!scope.info.isActive) {
+                            allMarkers[2].style.opacity = "1";
+                            allMarkers[2].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[2].style.opacity = "0.2";
+                            allMarkers[2].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;    
+                    case "Elands Bay": 
+                        if (!scope.info.isActive) {
+                            allMarkers[3].style.opacity = "1";
+                            allMarkers[3].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[3].style.opacity = "0.2";
+                            allMarkers[3].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Stillbaai": 
+                        if (!scope.info.isActive) {
+                            allMarkers[4].style.opacity = "1";
+                            allMarkers[4].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[4].style.opacity = "0.2";
+                            allMarkers[4].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Victoria Bay": 
+                        if (!scope.info.isActive) {
+                            allMarkers[5].style.opacity = "1";
+                            allMarkers[5].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[5].style.opacity = "0.2";
+                            allMarkers[5].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Cape St Francis": 
+                        if (!scope.info.isActive) {
+                            allMarkers[6].style.opacity = "1";
+                            allMarkers[6].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[6].style.opacity = "0.2";
+                            allMarkers[6].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Battery Beach": 
+                        if (!scope.info.isActive) {
+                            allMarkers[7].style.opacity = "1";
+                            allMarkers[7].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[7].style.opacity = "0.2";
+                            allMarkers[7].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Strand": 
+                        if (!scope.info.isActive) {
+                            allMarkers[8].style.opacity = "1";
+                            allMarkers[8].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[8].style.opacity = "0.2";
+                            allMarkers[8].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Big Bay": 
+                        if (!scope.info.isActive) {
+                            allMarkers[9].style.opacity = "1";
+                            allMarkers[9].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[9].style.opacity = "0.2";
+                            allMarkers[9].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Nahoon Reef": 
+                        if (!scope.info.isActive) {
+                            allMarkers[10].style.opacity = "1";
+                            allMarkers[10].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[10].style.opacity = "0.2";
+                            allMarkers[10].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Port Alfred": 
+                        if (!scope.info.isActive) {
+                            allMarkers[11].style.opacity = "1";
+                            allMarkers[11].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[11].style.opacity = "0.2";
+                            allMarkers[11].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                    case "Coffee Bay": 
+                        if (!scope.info.isActive) {
+                            allMarkers[12].style.opacity = "1";
+                            allMarkers[12].style.filter = "grayscale(0)";
+                            scope.info.isActive = true;
+                        } else {
+                            allMarkers[12].style.opacity = "0.2";
+                            allMarkers[12].style.filter = "grayscale(0.8)";
+                            scope.info.isActive = false;
+                        };
+                        break;
+                }
             };
         }
-    }
+    };
 });
